e10a3624ef2d733f3d9b9cd3d3489b6eb090e131
nikesh <nikesh.tadela@gmail.com>
2023-12-09 22:43:05 +0530

    added possible moves for pawn and highlighting squares

--- script/script.js
+++ script/script.js
@@ -1,9 +1,12 @@
 const boardHtml=document.querySelector(".board");
 const body=document.querySelector("body");
 let white_pov=true;
+let white_move=true;
 let clickedSqr=false;
 let sourceSqr,destinationSqr;
+let possibleMoves=[];
 let squares=[];
+let moves=[];
 let board=[
     [-5,-2,-3,-9,-10,-3,-2,-5],
     [-1,-1,-1,-1,-1,-1,-1,-1],
@@ -73,16 +76,44 @@ function sqrClickedListener(event){
         squares[sourceSqr[0]][sourceSqr[1]].innerHTML="";
         squares[destinationSqr[0]][destinationSqr[1]].innerHTML="";
         board[destinationSqr[0]][destinationSqr[1]]=board[sourceSqr[0]][sourceSqr[1]];
+        if(sourceSqr[0]!=destinationSqr[0]&&sourceSqr[1]!=destinationSqr[1]) board[sourceSqr[0]][sourceSqr[1]]=0;
         addImgToSqr(squares[destinationSqr[0]][destinationSqr[1]],board[destinationSqr[0]][destinationSqr[1]]);
+        moves.push([sourceSqr,destinationSqr]);
+        highlightSqrs(false);
         clickedSqr=false;
+        white_move=!white_move;
     }else{
         sourceSqr=findSqr(this);
-        if(board[sourceSqr[0]][sourceSqr[1]]){
+        if((board[sourceSqr[0]][sourceSqr[1]]>0&&white_move)||((board[sourceSqr[0]][sourceSqr[1]]<0&&!white_move))){
             clickedSqr=true;
+            movesFinder(sourceSqr,board[sourceSqr[0]][sourceSqr[1]]);
+            highlightSqrs(true);
+            console.log("piece held");
         }else{
             sourceSqr=undefined;
+            highlightSqrs(false);
             clickedSqr=false;
         }
         console.log(sourceSqr);
+    }
+}
+
+function highlightSqrs(yes){
+    for(let i=0;i<possibleMoves.length;i++){
+        squares[possibleMoves[i][0]][possibleMoves[i][1]].style.border=(yes)?"10px solid black":"";
+    }
+}
+
+function movesFinder(position,piece){
+    possibleMoves=[];
+    if(piece==1||piece==-1){
+        let r=position[0]-piece,c=position[1];
+        if(r>=0&&r<8&&c>=0&&c<8&&!board[r][c]) possibleMoves.push([r,c]);
+        if(c-1>=0&&board[r][c-1]) {possibleMoves.push([r,c-1]);console.log("can capture");}
+        if(c+1<8&&board[r][c+1]) {possibleMoves.push([r,c+1]);console.log("can capture");}
+        if(position[0]==1||position[0]==6){
+            r=position[0]-2*piece,c=position[1];
+            if(r>=0&&r<8&&c>=0&&c<8&&!board[r][c]) possibleMoves.push([r,c]);
+        }
     }
 }
